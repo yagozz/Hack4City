00e331538e3a07a4a32913dfa224533ed6177aa1
softwaredeveloper99 <softwaredeveloper99@outlook.com>
2017-10-07 00:42:41 +0300

    Initial commit

---
+++ app/src/main/java/com/hack4city/hack4city/WebService/WebService.java
@@ -0,0 +1,9 @@
+package com.hack4city.hack4city.WebService;
+public class WebService {
+    private String WebServiceBaseURL="https://hackhaton.izmir.bel.tr/api/";
+    private TokenModel tokenModel;
+
+    public WebService() {
+
+    }
+}
