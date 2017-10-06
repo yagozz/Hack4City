53a90b903c2f3f8aa5879b84997dace93a15a88c
ilaydacansinkoc <ilaydacansin@gmail.com>
2017-10-07 01:08:26 +0300

    Second Commit

---
+++ app/src/main/java/com/hack4city/hack4city/WebService/TramwayHoursModel.java
@@ -0,0 +1,46 @@
+package com.hack4city.hack4city.WebService;
+
+/**
+ * Created by Johnson on 7.10.2017.
+ */
+
+public class TramwayHoursModel {
+    private String BaslangicSaati;
+    private String BitisSaati;
+    private String Aralik;
+
+    public TramwayHoursModel() {
+        this("","","");
+    }
+
+    public TramwayHoursModel(String baslangicSaati, String bitisSaati, String aralik) {
+
+        BaslangicSaati = baslangicSaati;
+        BitisSaati = bitisSaati;
+        Aralik = aralik;
+    }
+
+    public String getBaslangicSaati() {
+        return BaslangicSaati;
+    }
+
+    public void setBaslangicSaati(String baslangicSaati) {
+        BaslangicSaati = baslangicSaati;
+    }
+
+    public String getBitisSaati() {
+        return BitisSaati;
+    }
+
+    public void setBitisSaati(String bitisSaati) {
+        BitisSaati = bitisSaati;
+    }
+
+    public String getAralik() {
+        return Aralik;
+    }
+
+    public void setAralik(String aralik) {
+        Aralik = aralik;
+    }
+}
